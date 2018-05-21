b782cd7a501a0ad6f91e9cc01f811646ecb6d321
Mahesh Krishna Kumar <mahesh@sv.co>
2018-05-21 16:17:32 +0530

    AA: Create group filter for user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,16 +2,18 @@ ActiveAdmin.register User do
   controller do
     actions :all, except: [:edit, :destroy]
   end
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :group_id
 
   index do
     selectable_column
     id_column
     column :email
+    column :group
     actions
   end
 
   filter :email
+  filter :group
 
   form do |f|
     f.inputs do

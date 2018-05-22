cc46d39956135010069a2ceda6e21b6874cbfce7
Mahesh Krishna Kumar <mahesh@sv.co>
2018-05-23 00:09:45 +0530

    Fix prediction update route

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -4,9 +4,14 @@ class PredictionsController < ApplicationController
     head :ok if prediction.save!
   end
 
+  def update
+    prediction = Prediction.find_by(match_id: prediction_params[:match_id])
+    head :ok if prediction.update!(winner_id: prediction_params[:winner_id])
+  end
+
   private
 
   def prediction_params
-    params.require(:prediction).permit(:match_id, :winner_id)
+    @prediction_params ||= params.require(:prediction).permit(:match_id, :winner_id)
   end
 end

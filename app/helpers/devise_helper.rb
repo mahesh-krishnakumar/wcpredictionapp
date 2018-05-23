99d2970a8a087fc032117c33e58eb85c42b1a8f6
Abdul Jaleel <jaleel@sv.co>
2018-05-23 13:13:17 +0530

    Display error messages if login fails
    Uses an ugly approach to get this working with Devise.

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,22 @@
+module DeviseHelper
+  def devise_login_error_messages!
+    flash_alerts = []
+
+    if !flash.empty?
+      flash_alerts.push(flash[:error]) if flash[:error]
+      flash_alerts.push(flash[:alert]) if flash[:alert]
+      flash_alerts.push(flash[:notice]) if flash[:notice]
+    end
+
+    return "" if flash_alerts.empty?
+    messages = flash_alerts.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+    <div id="error_explanation" class="alert alert-danger">
+      <ul class="mb-0 pl-3">#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end

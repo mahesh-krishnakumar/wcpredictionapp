ae361c783cbe28f6b4452032b46e2a3ee6980fbf
Mahesh Krishna Kumar <mahesh@sv.co>
2018-05-24 12:39:40 +0530

    Compute winner from prediction goals

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -8,4 +8,9 @@ class Prediction < ApplicationRecord
   validates :team_1_goals, presence: true
   validates :team_2_goals, presence: true
   validates :decider, inclusion: { in: Match.valid_decider_types }, allow_nil: true
+
+  def winner
+    return if team_1_goals == team_2_goals
+    team_1_goals > team_2_goals ? match.team_1 : match.team_2
+  end
 end

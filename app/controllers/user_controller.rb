d3c836ad953e9270feaa4b44a7cbb489d1ad85c6
Abdul Jaleel <jaleel@sv.co>
2018-05-28 15:22:43 +0530

    Add a basic leaderboard page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,4 +6,10 @@ class UserController < ApplicationController
     @completed_matches = Match.completed.group_by_day { |m| m.kick_off }
     @results = Matches::PredictionResultService.new(current_user.group).results
   end
+
+  def leaderboard
+    @group = current_user.group
+    @users = @group.users
+    @leaderboard = Groups::StandingsTableService.new(@group).table
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'user#dashboard'
+  get 'leaderboard', to: 'user#leaderboard', as: 'leaderboard'
 
   devise_for :users, skip: %i(sessions registrations)
   as :user do

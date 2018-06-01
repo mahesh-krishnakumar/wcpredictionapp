19fcb52fc49b7ee947d4819160c0fc8fec8200ea
Mahesh Krishna Kumar <mahesh@sv.co>
2018-06-01 12:31:14 +0530

    Add winner_id to predictions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_083056) do
+ActiveRecord::Schema.define(version: 2018_06_01_063025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,8 +99,10 @@ ActiveRecord::Schema.define(version: 2018_05_29_083056) do
     t.datetime "updated_at", null: false
     t.integer "team_1_goals"
     t.integer "team_2_goals"
+    t.bigint "winner_id"
     t.index ["match_id"], name: "index_predictions_on_match_id"
     t.index ["user_id"], name: "index_predictions_on_user_id"
+    t.index ["winner_id"], name: "index_predictions_on_winner_id"
   end
 
   create_table "teams", force: :cascade do |t|

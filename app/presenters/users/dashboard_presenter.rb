d51e0c541be87366ede79380cbafd8613d69d5dd
Mahesh Krishna Kumar <mahesh@sv.co>
2018-06-05 16:01:08 +0530

    Sort match dates for the datestrip

--- app/presenters/users/dashboard_presenter.rb
+++ app/presenters/users/dashboard_presenter.rb
@@ -56,7 +56,7 @@ module Users
     end
 
     def match_dates
-      Match.unlocked.pluck(:kick_off).map(&:to_date)
+      Match.unlocked.pluck(:kick_off).map(&:to_date).sort!
     end
 
     def unlocked_matches

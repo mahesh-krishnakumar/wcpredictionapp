d7b6350990c88a69a4e223f8f468a31c93208781
Mahesh Krishna Kumar <mahesh@sv.co>
2018-06-06 20:02:29 +0530

    Temporary hack to seed matches alone

--- db/seeds/matches.seeds.rb
+++ db/seeds/matches.seeds.rb
@@ -1,23 +1,22 @@
-after 'teams' do
-  puts 'Seeding Matches'
+puts 'Seeding Matches'
 
-  require 'csv'
+require 'csv'
 
-  fixture_csv = File.read(Rails.root.join('WCFixtures_UTC.csv'))
+fixture_csv = File.read(Rails.root.join('WCFixtures_UTC.csv'))
 
-  csv = CSV.parse(fixture_csv, headers: true)
+csv = CSV.parse(fixture_csv, headers: true)
 
-  csv.each do |row|
-    match_details = row.to_hash
-    Match.create!(
-      team_1: Team.find_by(full_name: match_details['team_1']),
-      team_2: Team.find_by(full_name: match_details['team_2']),
-      venue: match_details['venue'],
-      kick_off: DateTime.parse(match_details['kick_off']),
-      stage: Match::STAGE_GROUP
-    )
-  end
-
-  # and `unlock` just the first six matches for now
-  Match.all.limit(6).update_all(locked: false)
+csv.each do |row|
+  match_details = row.to_hash
+  Match.create!(
+    team_1: Team.find_by(full_name: match_details['team_1']),
+    team_2: Team.find_by(full_name: match_details['team_2']),
+    venue: match_details['venue'],
+    kick_off: DateTime.parse(match_details['kick_off']),
+    stage: Match::STAGE_GROUP
+  )
 end
+
+# and `unlock` just the first six matches for now
+Match.all.limit(6).update_all(locked: false)
+

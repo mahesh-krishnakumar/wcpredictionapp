1c53018f3942a9b5d0ec5301503f68338f335e94
Abdul Jaleel <jaleel@sv.co>
2018-06-11 19:43:39 +0530

    Allow admin to modify group code

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register Group do
-  permit_params :name
+  permit_params :name, :code
 
   filter :name
 
   form do |f|
     f.inputs do
       f.input :name
+      f.input :code
     end
     f.actions
   end

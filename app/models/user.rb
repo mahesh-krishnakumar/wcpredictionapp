40af935cde9ee8d491aab8c30ff3784ef0088421
Mahesh Krishna Kumar <mahesh@sv.co>
2018-06-29 13:16:01 +0530

    Fix bug in user callbacks for recomputation of ranks

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :predictions
-  has_and_belongs_to_many :groups
+  has_and_belongs_to_many :groups, after_add: :update_ranks_and_points, after_remove: :update_ranks_and_points
   belongs_to :team, optional: true # her favourite team
 
   # validates :group, presence: true
   validates :name, presence: true
   validates :nick_name, presence: true
 
-  after_save :update_ranks_and_points
+  after_create :update_ranks_and_points
 
-  def update_ranks_and_points
+  def update_ranks_and_points(_group = nil)
     Users::UpdateRanksService.new.execute
   end
 end

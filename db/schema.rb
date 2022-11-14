5c119f67e5cacb946e393a5c06aed457b79ab2e1
Mahesh Krishna Kumar <maheshcet@gmail.com>
2022-11-14 15:42:57 +0530

    Add WC2022 data

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 2018_06_24_172010) do
     t.bigint "resource_id"
     t.string "author_type"
     t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
@@ -92,8 +92,8 @@ ActiveRecord::Schema.define(version: 2018_06_24_172010) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stage"
-    t.boolean "locked", default: true
     t.bigint "winner_id"
+    t.boolean "locked", default: true
     t.string "summary_text"
     t.string "short_text"
     t.index ["winner_id"], name: "index_matches_on_winner_id"
--- db/seeds/matches.seeds.rb
+++ db/seeds/matches.seeds.rb
@@ -2,12 +2,13 @@ puts 'Seeding Matches'
 
 require 'csv'
 
-fixture_csv = File.read(Rails.root.join('uefa-euro-2020-UTC.csv'))
+fixture_csv = File.read(Rails.root.join('fifa-world-cup-2022.csv'))
 
 csv = CSV.parse(fixture_csv, headers: true)
 
 csv.each do |row|
   match_details = row.to_hash
+  binding.pry
   Match.create!(
     team_1: Team.find_by(full_name: match_details['team_1']),
     team_2: Team.find_by(full_name: match_details['team_2']),
@@ -19,4 +20,3 @@ end
 
 # and `unlock` just the first six matches for now
 Match.all.limit(6).update_all(locked: false)
-

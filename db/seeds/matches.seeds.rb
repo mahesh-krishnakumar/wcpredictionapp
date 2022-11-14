b404ecb5f963f4dea9bad241ed6243eb45685ab3
Mahesh Krishna Kumar <maheshcet@gmail.com>
2022-11-14 15:54:44 +0530

    Cleanup pry

--- db/seeds/matches.seeds.rb
+++ db/seeds/matches.seeds.rb
@@ -8,7 +8,6 @@ csv = CSV.parse(fixture_csv, headers: true)
 
 csv.each do |row|
   match_details = row.to_hash
-  binding.pry
   Match.create!(
     team_1: Team.find_by(full_name: match_details['team_1']),
     team_2: Team.find_by(full_name: match_details['team_2']),
